af92f5dc3a3291fec9fc5372d4aa6c856ca67171
Miflop <Miflop@users.noreply.github.com>
2021-10-02 09:54:42 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -5,4 +5,7 @@ iptables -F && iptables -X
 iptables -t nat -F && iptables -t nat -X
 iptables -t raw -F && iptables -t raw -X
 iptables -t mangle -F && iptables -t mangle -X
-systemctl restart docker
+sudo systemctl daemon-reload
+# remove all running docker containers
+docker rm -f `docker ps -a | grep "k8s_" | awk '{print $1}'`
+sudo systemctl restart docker
